3dd16e6cfe91ce5adf7a8e171ba236cf1ac65f54
Kurov-Oleg <74539138+Kurov-Oleg@users.noreply.github.com>
2022-05-24 11:15:22 +0300

    Update backet.tf

--- 15.3/backet.tf
+++ 15.3/backet.tf
@@ -5,7 +5,24 @@ provider "yandex" {
   zone      = "ru-central1-a"
 }
 
+resource "yandex_kms_symmetric_key" "key-a" {
+  name              = "secret"
+  description       = ""
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h" 
+}
+
 resource "yandex_storage_bucket" "test" {
   access_key = "abjm2ihhf6h3fdqffct2"
   bucket = "netology-kurov-2022"
+  secret_key="secret"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
+
